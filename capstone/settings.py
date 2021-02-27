28f73b096dc0286a57312575a890f61e6900b4a7
Jeanine Beckle <jeaninebeckle@gmail.com>
2021-02-27 12:13:00 -0600

    minor cleanup

--- capstone/settings.py
+++ capstone/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z_stcyld-%%y5goq5h@%%m)0a0na8kkg#xh)s!i13@bsqiaw%m'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
