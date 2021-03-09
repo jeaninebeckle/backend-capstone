639f2d450e17be7d755e14c9ed39dd0d33efb339
Jeanine Beckle <jeaninebeckle@gmail.com>
2021-03-08 21:32:31 -0600

    calendar text can be edited

--- capstoneapi/views/caltext.py
+++ capstoneapi/views/caltext.py
@@ -23,6 +23,32 @@ class CalTextViewSet(ViewSet):
       caltexts, many=True, context={'request': request})
     return Response(serializer.data)
 
+  def retrieve(self, request, pk=None):
+    """Handle GET requests for single text
+
+    returns:
+      Response -- JSON serialized text
+    """
+    try:
+      text = CalendarText.objects.get(pk=pk)
+      serializer = CalTextSerializer(text, context={'request': request})
+      return Response(serializer.data)
+    except Exception as ex:
+      return HttpResponseServerError(ex) 
+
+  def update(self, request, pk=None):
+      """Handle PUT requests for cal text
+
+      Returns:
+          Response -- Empty body with 204 status code
+      """
+      calText = CalendarText.objects.get(pk=pk)
+      calText.content = request.data["content"]
+  
+      calText.save()
+
+      return Response({}, status=status.HTTP_204_NO_CONTENT)
+
   
 class CalTextSerializer(serializers.ModelSerializer):
   """JSON Serializer for calendar text
